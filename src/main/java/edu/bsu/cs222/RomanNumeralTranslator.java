93f363d6f9723a653b14b8f4c6ffe6048b387acb
Chandler Clevenger <cjclevenger@bsu.edu>
2021-03-03 20:37:17 -0500

    translates roman numeral symbol to numerical value
    created a conversion between each roman numeral symbol and its corresponding numerical value

---
+++ src/main/java/edu/bsu/cs222/RomanNumeralTranslator.java
@@ -0,0 +1,22 @@
+package edu.bsu.cs222;
+
+public class RomanNumeralTranslator {
+    public int convertRomanNumeralValue(char romanNumeral) {
+        if (romanNumeral == 'i') {
+            return 1;
+        } else if (romanNumeral == 'v') {
+            return 5;
+        } else if (romanNumeral == 'x') {
+            return 10;
+        } else if (romanNumeral == 'l') {
+            return 50;
+        } else if (romanNumeral == 'c') {
+            return 100;
+        } else if (romanNumeral == 'd') {
+            return 500;
+        } else if (romanNumeral == 'm') {
+            return 1000;
+        }
+        return 0;
+    }
+}
---
+++ src/test/java/edu/bsu/cs222/RomanNumeralTranslatorTest.java
@@ -0,0 +1,15 @@
+package edu.bsu.cs222;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class RomanNumeralTranslatorTest {
+    @ParameterizedTest
+    @CsvSource({"i,1", "v,5", "x,10", "l,50", "c,100", "d,500", "m,1000"})
+    public void testTranslate(char romanNumeral, int value) {
+        RomanNumeralTranslator translator = new RomanNumeralTranslator();
+        int actual = translator.convertRomanNumeralValue(romanNumeral);
+        Assertions.assertEquals(value, actual);
+    }
+}
