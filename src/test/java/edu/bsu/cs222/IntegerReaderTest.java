f39bd3ffa8ca2ccf71b41377b51d8925ccde8c70
Chandler Clevenger <cjclevenger@bsu.edu>
2021-03-04 10:54:58 -0500

    reformatted code

--- src/test/java/edu/bsu/cs222/IntegerReaderTest.java
+++ src/test/java/edu/bsu/cs222/IntegerReaderTest.java
@@ -6,9 +6,9 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class IntegerReaderTest {
     @ParameterizedTest
-    @CsvSource({"1,i","2,ii","3,iii","4,iv","5,v","6,vi","7,vii","8,viii","9,ix",
-            "10,x","99,xcix","100,c","3000,mmm","2999,mmcmxcix"})
-    public void TestNumberTranslate(int value,String romanNumeral){
+    @CsvSource({"1,i", "2,ii", "3,iii", "4,iv", "5,v", "6,vi", "7,vii", "8,viii", "9,ix",
+            "10,x", "99,xcix", "100,c", "3000,mmm", "2999,mmcmxcix"})
+    public void TestNumberTranslate(int value, String romanNumeral) {
         IntegerReader integerReader = new IntegerReader();
         String actual = integerReader.read(value);
         Assertions.assertEquals(romanNumeral, actual);
--- src/test/java/edu/bsu/cs222/RomanNumeralReaderTest.java
+++ src/test/java/edu/bsu/cs222/RomanNumeralReaderTest.java
@@ -6,8 +6,8 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class RomanNumeralReaderTest {
     @ParameterizedTest
-    @CsvSource({"i,1","ii,2","iii,3","iv,4","v,5","vi,6","vii,7","viii,8","ix,9",
-            "x,10","xcix,99","c,100","mmm,3000","mmcmxcix,2999","clxxxix, 189" })
+    @CsvSource({"i,1", "ii,2", "iii,3", "iv,4", "v,5", "vi,6", "vii,7", "viii,8", "ix,9",
+            "x,10", "xcix,99", "c,100", "mmm,3000", "mmcmxcix,2999", "clxxxix, 189"})
     public void testTranslate(String romanNumeral, int value) {
         RomanNumeralReader romanNumeralReader = new RomanNumeralReader();
         int actual = romanNumeralReader.read(romanNumeral);
